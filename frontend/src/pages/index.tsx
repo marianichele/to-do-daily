4f62f5d0f63b33cfc84542b846441eb4aef5632b
Maria Eduarda <maria.cnichele@gmail.com>
2022-07-10 20:11:33 -0300

    eslint config

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+import React from 'react';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+  return <div className={styles.container}>Oi</div>;
+};
+
+export default Home;
